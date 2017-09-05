c858d5e400f61648e1bdc201ce23317b71241c19
Russ Hammett <Kritner@gmail.com>
2017-09-05 14:46:56 -0400

    sample code

---
+++ ConsoleMixingNetStandardAndCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using StandardLibrary;
+
+namespace ConsoleMixingNetStandardAndCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 thingy = new Class1();
+            Console.WriteLine(thingy.GetTextToOutput());
+        }
+    }
+}
---
+++ StandardLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StandardLibrary
+{
+    public class Class1
+    {
+        public string GetTextToOutput()
+        {
+            return "Hello standard world!";
+        }
+    }
+}
